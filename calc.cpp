4f146b324980935b1171e1428d658f0754e449a3
Jhe0031 <jackie.he8@gmail.com>
2019-02-07 12:49:00 -0500

    Added comments

--- calc.cpp
+++ calc.cpp
@@ -2,23 +2,22 @@
 using namespace std;
 
 int main() { // Creating main function
-	int sum, num;
-	char oper;
+	int sum = 0, num; // Declaring integer variables sum with a value -, and integer variable num
+	char oper; // Declaring a character variable named oper 
 
-	while(cin >> num) {
-		// cout << "num " << num << endl;
-
-		if (oper == '+') {
-			sum = sum + num;
-		} else if (oper == '-') {
-			sum = sum - num;
-		} else {
+	while(cin >> num) { // While loop in order to iterate through formula.txt
+		if (oper == '+') { // If loop
+			// If the operation is addition
+			sum = sum + num; // Add the number to the variable sum
+		} else if (oper == '-') { // else if operation is subtraction
+			sum = sum - num; // Subtract number
+		} else { // Otherwise, add number
 			sum = sum + num;
 		}
 
-		cin >> oper;
+		cin >> oper; // After the first loop, store the operation from formula.txt into the variable oper
 	}
 
-	cout << sum << endl;
+	cout << sum << endl; // Prints the sum
 	return sum;
 }
