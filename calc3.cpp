f7c5649ae03f53018009b50086dfa2b782fe9547
Jhe0031 <jackie.he8@gmail.com>
2019-02-18 23:46:17 -0500

    Fixed Project 1D

--- calc3.cpp
+++ calc3.cpp
@@ -1,52 +1,37 @@
 #include <iostream>
+
 using namespace std;
 
-int main() {
-	int sum = 0, num; // Declaring integer variables sum with a value -, and integer variable num
-	char oper; // Declaring a character variable named oper 
+int sum = 0, num; // Declaring integer variables sum with a value -, and integer variable num
+char oper; // Declaring a character variable named oper 
 
-	while(cin >> num) { 
-		if (oper == '+') { // If the operation is addition
-			cin >> oper; // Checks the next operation
-			if (oper == '^') { // Check if the number is squared
-				num = num*num; // Squares the number
-			}
+int exp() {
+	cin >> oper; // Checks the next operation
+	if (oper == '^') { // Checks if the number is squared
+		num = num*num; // Squares the number 
+		cin >> oper; // Checks the next operation
+	}
+}
+
+int main() {
+	while(cin >> num) { // Creates a while loop to read the file into the variable num
+		if (oper == '+') { // When the operation is readable as the addition sign
+			exp(); // Calls exp function
 			sum = sum + num; // Add the number to the variable sum
-			cin >> oper; // Checks the next operation
-			if (oper == ';') { // Checks for end of line
-				cout << sum << endl; // Prints sum
-				sum = 0; // Resets the sum
-				cin >> num; // Saves next number
-				cin >> oper; // Checks the next operation
-			}
 		} else if (oper == '-') { // else if operation is subtraction
-			cin >> oper; // Checks the next operation
-			if (oper == '^') { // Check if the number is squared
-				num = num*num; // Squares the number
-			}
+			exp(); // Calls exp function
 			sum = sum - num; // Subtract number
-			cin >> oper; // Checks the next operation
-			if (oper == ';') { // Checks for end of line
-				cout << sum << endl; // Prints sum
-				sum = 0; // Resets the sum
-				cin >> num; // Saves next number
-				cin >> oper; // Checks the next operation
-			}
-		} else {
-			cin >> oper; // Checks the next operation
-			if (oper == '^') { // Checs if the number is squared
-				num = num*num; // Squares the number
-			}
+		} else { // Everything else will run this
+			exp(); // Calls exp function
 			sum = sum + num; // Otherwise, add number
-			cin >> oper; // Checks the next operation
-			if (oper == ';') { // Checks for end of line
-				cout << sum << endl; // Prints sum
-				sum = 0; // Resets the sum
-				cin >> num; // Saves next number
-				sum = sum + num; // Adds number
-				cin >> oper; // Checks the next operation
-			}
+		}
+
+		if (oper == ';') { // When the operation is a semicolon
+			cout << sum << endl; // Prints out the sum
+			sum = 0; // Resets the sum value
+			oper = ' '; // Resets the operation value
+		 // After the first loop, store the operation from formula.txt into the variable oper
 		}
 	}
-	return 0; // Returns 0 otherwise
+	return 0;
 }
